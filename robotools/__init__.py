0fe4c55dcaaab1040e71e6d938fc3aad6b197350
Michael Osthege <m.osthege@fz-juelich.de>
2021-07-28 15:37:23 +0200

    Expose helper functions to create well arrays/dicts
    Closes https://github.com/JuBiotech/robotools/issues/5

--- robotools/__init__.py
+++ robotools/__init__.py
@@ -3,6 +3,6 @@ from . import evotools
 from . evotools import InvalidOperationError, Labwares, Tip, Worklist
 from . liquidhandling import Labware, Trough, VolumeOverflowError, VolumeUnderflowError
 from . utils import DilutionPlan, get_trough_wells
-from . transform import WellShifter, WellRotator
+from . transform import WellShifter, WellRotator, make_well_array, make_well_index_dict
 
-__version__ = '1.1.0'
+__version__ = '1.2.0'
