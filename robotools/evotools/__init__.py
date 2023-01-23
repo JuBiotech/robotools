ac14c31affd2fecfd01185dba882b65e8c2d9a30
j.niesser <j.niesser@fz-juelich.de>
2023-01-23 15:45:38 +0100

    apply rounding to volume of aspirate/dispense

--- robotools/evotools/__init__.py
+++ robotools/evotools/__init__.py
@@ -271,6 +271,9 @@ def _prepare_evo_aspirate_dispense_parameters(
         volume = volume * len(wells)
     else:
         raise ValueError(f"Invalid volume: {volume}")
+    
+    # apply rounding and corrections for the right string formatting
+    volume = [numpy.round(vol, decimals=2) for vol in volume]
 
     if liquid_class is None:
         raise ValueError(f"Missing required parameter: liquid_class")
