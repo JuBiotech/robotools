853ff69f0197264a5471b178b7f70e8bd146cd1f
Michael Osthege <m.osthege@fz-juelich.de>
2023-05-05 18:52:15 +0200

    Upgrade metadata and CI versions up to Python 3.11

--- setup.py
+++ setup.py
@@ -59,9 +59,10 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "License :: OSI Approved :: GNU Affero General Public License v3",
         "Intended Audience :: Science/Research",
         "Topic :: Scientific/Engineering",
