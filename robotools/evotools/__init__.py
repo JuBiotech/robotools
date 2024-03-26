f8e5c617e99bf34f8602ed987e15c387ed342f53
Michael Osthege <m.osthege@fz-juelich.de>
2024-03-26 14:50:21 +0100

    Add functions for calculating model-specific well position numbers
    This prepares deprecation of the `Labware.positions` lookup dictionary
    by providing an alternative, model-specific way to get position numbers.
    These functions account for differences in how EVO vs. Fluent
    address well positions in troughs.

--- robotools/evotools/__init__.py
+++ robotools/evotools/__init__.py
@@ -1,3 +1,4 @@
 from robotools.evotools.types import Labwares, Tip, int_to_tip
+from robotools.evotools.utils import get_well_position
 from robotools.evotools.worklist import EvoWorklist, Worklist
 from robotools.worklists.exceptions import InvalidOperationError
