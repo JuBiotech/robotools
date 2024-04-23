2feac6a7479b7faa9d17b8f73588eb4bb257e5af
Michael Osthege <m.osthege@fz-juelich.de>
2024-04-23 12:57:48 +0200

    Switch to the PyData theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,8 +15,6 @@
 # sys.path.insert(0, os.path.abspath('.'))
 from typing import List
 
-import sphinx_rtd_theme
-
 from robotools import __version__
 
 # -- Project information -----------------------------------------------------
@@ -36,10 +34,10 @@ release = __version__
 # ones.
 extensions = [
     "sphinx.ext.autodoc",
-    "sphinx_rtd_theme",
     "numpydoc",
     "nbsphinx",
     "nbsphinx_link",
+    "pydata_sphinx_theme",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -56,7 +54,7 @@ exclude_patterns: List[str] = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+html_theme = "pydata_sphinx_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
