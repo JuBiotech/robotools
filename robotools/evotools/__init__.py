612b18bbe1b73ca732b5f2a450edfffb52b50064
Michael Osthege <m.osthege@fz-juelich.de>
2025-08-14 09:39:55 +0200

    Remove deprecated `Worklist` type

--- robotools/evotools/__init__.py
+++ robotools/evotools/__init__.py
@@ -1,4 +1,4 @@
 from robotools.evotools.types import Labwares, Tip, int_to_tip
 from robotools.evotools.utils import get_well_position
-from robotools.evotools.worklist import EvoWorklist, Worklist
+from robotools.evotools.worklist import EvoWorklist
 from robotools.worklists.exceptions import InvalidOperationError
